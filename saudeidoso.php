168ab6ba808213b436e358c4e40de2c81f129c70
Lana Araújo <125031682+Lana-Araujo@users.noreply.github.com>
2024-11-07 21:50:53 -0300

    saude

--- saudeidoso.php
+++ saudeidoso.php
@@ -27,7 +27,7 @@
             <h2>Saúde da pessoa idosa</h2>
             <p>A saúde da pessoa idosa envolve o cuidado e o bem-estar de uma população em envelhecimento que possui necessidades específicas e frequentemente complexas.</p>
             <p>Com o aumento da expectativa de vida, é essencial adotar estratégias e práticas de cuidado que promovam não apenas a longevidade, mas também a qualidade de vida dos idosos.</p>
-            <button class="button">Saiba Mais</button>
+           
         </div>
         <div class="hero-image">
             <img src="img/saudeidoso4.png" alt="Imagem de destaque" >
@@ -60,35 +60,64 @@
 
     <div class="green-box">
         <h2>Legislação Brasileira sobre saúde da pessoa idosa</h2>
-        <p>É a principal legislação no Brasil sobre os direitos das pessoas idosas. A Política Nacional de Saúde da Pessoa Idosa (Portaria 2.528/2006) estabelece diretrizes para o cuidado e prevenção de doenças e as normas da ANS garantem acesso facilitado a serviços de saúde para idosos em planos de saúde.</p>
+        <p>É a principal legislação no Brasil sobre os direitos das pessoas idosas. A Política Nacional de Saúde da Pessoa Idosa (Portaria 2.528/2006) estabelece diretrizes para o cuidado e prevenção de doenças, além disso, as normas da ANS garantem um acesso facilitado a serviços de saúde para idosos em planos de saúde.</p>
         <div class="info-box-green">
             <span>Estatuto do Idoso (Lei nº 10.741/2003)</span>
         </div>
     </div>
-
     <section class="info-section">
-        <h2>Doença de Alzheimer</h2>
-        <div class="info-item">
-            <span class="item-number">1.</span>
-            <div class="info-content">
-                <h3>Aumento da Incidência</h3>
-                <p>A Doença de Alzheimer é uma forma comum de demência, afetando cerca de 10% das pessoas acima de 65 anos, e sua incidência está crescendo devido ao envelhecimento da população.</p>
+    <h2> Atividade Física na prevenção de doenças</h2>
+    <div class="info-item">
+        <span class="item-number">1.</span>
+        <div class="info-content">
+            <h3>Qualidade de vida</h3>
+            <p>A prática regular de atividade física contribui para a prevenção de diversas doenças, incluindo doenças cardiovasculares, diabetes e a Doença de Alzheimer, que é uma das formas mais comuns de demência entre pessoas acima de 65 anos.</p>
+        </div>
+    </div>
+    <div class="info-item">
+        <span class="item-number">2.</span>
+        <div class="info-content">
+            <h3>Benefícios para a Saúde Mental</h3>
+            <p>Além de melhorar a saúde física, a atividade física regular ajuda a manter a saúde mental, proporcionando bem-estar, reduzindo o risco de depressão e ajudando a preservar as funções cognitivas, que são essenciais na prevenção do Alzheimer.</p>
+        </div>
+    </div>
+    <div class="info-item">
+        <span class="item-number">3.</span>
+        <div class="info-content">
+            <h3>Estilo de Vida Saudável</h3>
+            <p>Adotar um estilo de vida que inclui exercícios, uma dieta equilibrada e o estímulo cognitivo pode reduzir significativamente os riscos de doenças crônicas e degenerativas. Para idosos, esses hábitos são fundamentais para manter a independência e a qualidade de vida.</p>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="footer-content">
+        <div class="footer-logo">
+            <img src="img/logo.png" alt="Vitalis Logo">
+            <p>A Vitalis é uma plataforma inovadora que se dedica a atender as necessidades da população idosa, oferecendo uma gama de serviços e recursos essenciais.</p>
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
             </div>
         </div>
-        <div class="info-item">
-            <span class="item-number">2.</span>
-            <div class="info-content">
-                <h3>Causas e Fatores de Risco</h3>
-                <p>As causas incluem fatores genéticos, ambientais e de estilo de vida. Riscos são idade avançada, histórico familiar, predisposições genéticas e condições como doenças cardiovasculares e diabetes.</p>
-            </div>
+        <div class="footer-links">
+            <h4>Links</h4>
+            <a href="#">Home</a>
+            <a href="#">Serviços</a>
+            <a href="#">Feedbacks</a>
+            <a href="#">Entrar</a>
         </div>
-        <div class="info-item">
-            <span class="item-number">3.</span>
-            <div class="info-content">
-                <h3>Prevenção e Tratamento</h3>
-                <p>Não há cura, mas um estilo de vida saudável, incluindo exercícios, dieta equilibrada e estímulo cognitivo, pode ajudar na prevenção. Medicamentos como inibidores da colinesterase são usados para tratar sintomas, juntamente com terapia ocupacional e suporte psicológico.</p>
-            </div>
+        <div class="footer-legal">
+            <h4>Legal</h4>
+            <a href="#">Termos de uso</a>
         </div>
-    </section>
+    </div>
+  
+    <div class="footer-copyright">
+        Copyright 2024, Todos os direitos reservados
+    </div>
+</footer>
+
+
 </body>
 </html>
